52b0e56fd0d37ebf58eeb318438e60beda6e9ba2
pirupiruntha <spiruntha1116@gmail.com>
2023-03-29 08:36:21 +0530

    add crossOrigin annotation

--- src/main/java/org/piruntha/controllers/EmployeeController.java
+++ src/main/java/org/piruntha/controllers/EmployeeController.java
@@ -13,6 +13,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/employees")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
