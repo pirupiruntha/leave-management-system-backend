3946db89fcf5f4bfba995c276c54014b1ab555f5
pirupiruntha <spiruntha1116@gmail.com>
2023-04-03 17:18:05 +0530

    create AuthService

---
+++ src/main/java/org/piruntha/services/AuthService.java
@@ -0,0 +1,56 @@
+package org.piruntha.services;
+
+import org.piruntha.dto.requests.AuthRequest;
+import org.piruntha.dto.responses.LoginResponse;
+import org.piruntha.jwt.JwtService;
+import org.piruntha.model.Employee;
+import org.piruntha.model.UserRoles;
+import org.piruntha.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class AuthService {
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+
+    public LoginResponse authenticateUser(AuthRequest authRequest) {
+        //authenticate the user
+        Authentication authenticate = authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(authRequest.getUsername(), authRequest.getPassword()));
+
+        if (authenticate.isAuthenticated()){
+            //generate JWT
+            String token = jwtService.generateToken(authRequest.getUsername());
+            //get user from DB
+            Optional<Employee> user = employeeRepository.findEmployeeByUsername(authRequest.getUsername());
+
+            //set user roles to a list from user retrieved from DB
+            List<UserRoles> role = user.get().getRoles();
+            //create a new LoginResponse object with the generated token and role list
+            LoginResponse loginResponse = new LoginResponse();
+            loginResponse.setToken(token);
+            loginResponse.setRoles(role);
+            return loginResponse;
+        }else {
+            throw new UsernameNotFoundException("Invalid user request ! ");
+        }
+    }
+}
