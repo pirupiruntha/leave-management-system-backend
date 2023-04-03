2058dc86d4993aafc6618e7d33cd6c09ced363de
pirupiruntha <spiruntha1116@gmail.com>
2023-04-03 14:50:30 +0530

    change method

--- src/main/java/org/piruntha/repository/EmployeeRepository.java
+++ src/main/java/org/piruntha/repository/EmployeeRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import java.util.Optional;
 
 public interface EmployeeRepository extends MongoRepository<Employee, String> {
-    Optional<Employee> findEmployeeByFullName(String fullName);
+    Optional<Employee> findEmployeeByUsername(String username);
+
 }
 
