d4adc691ecb79b0a0493e88e1f9a5a3460db0d5a
pirupiruntha <spiruntha1116@gmail.com>
2023-04-10 23:57:31 +0530

    change data type to localDate

--- src/main/java/org/piruntha/model/Leave.java
+++ src/main/java/org/piruntha/model/Leave.java
@@ -6,7 +6,8 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import java.util.Date;
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,76 +16,14 @@ public class Leave {
     @Id
     private String id;
     private String empUsername;
-    private String leaveType;
-    private Date startDate;
-    private Date endDate;
+    private LocalDate startDate;
+    private LocalDate endDate;
     private boolean halfDay;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getEmpUsername() {
-        return empUsername;
-    }
+    private  String reason;
+    private String status;
 
     public void setEmpUsername(String empUsername) {
         this.empUsername = empUsername;
     }
-
-    public String getLeaveType() {
-        return leaveType;
-    }
-
-    public void setLeaveType(String leaveType) {
-        this.leaveType = leaveType;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    public boolean isHalfDay() {
-        return halfDay;
-    }
-
-    public void setHalfDay(boolean halfDay) {
-        this.halfDay = halfDay;
-    }
-
-    public String getReason() {
-        return reason;
-    }
-
-    public void setReason(String reason) {
-        this.reason = reason;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    private  String reason;
-    private String status;
 
 }
